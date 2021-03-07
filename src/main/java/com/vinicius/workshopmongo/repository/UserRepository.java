cb076fa54ce60545acbac574a9b71022355b8e9b
Vinicius Bastos <vinicius_hora@live.com>
2021-03-07 17:17:32 -0300

    atualização de usuário com PUT

--- src/main/java/com/vinicius/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/vinicius/workshopmongo/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.vinicius.workshopmongo.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,10 @@ import com.vinicius.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+	User save(Optional<User> newObj);
+
+	
 
 	
 
