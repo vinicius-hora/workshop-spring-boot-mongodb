82ccb58535e6795e3e6e0387787f70d0c311b613
Vinicius Bastos <vinicius_hora@live.com>
2021-03-27 12:02:43 -0300

    Referenciando os posts do usuário

--- src/main/java/com/vinicius/workshopmongo/domain/User.java
+++ src/main/java/com/vinicius/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.vinicius.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -15,15 +18,28 @@ public class User implements Serializable{
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
 		
 	}
+
+	
 
 	public User(String id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	public String getId() {
