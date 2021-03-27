ca587fd3733e8ccb14038b17c9ce6fc43de36a9a
Vinicius Bastos <vinicius_hora@live.com>
2021-03-27 11:53:26 -0300

    Projeção dos dados com autor DTO

--- src/main/java/com/vinicius/workshopmongo/domain/Post.java
+++ src/main/java/com/vinicius/workshopmongo/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.vinicius.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -15,7 +17,7 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public String getId() {
 		return id;
@@ -41,14 +43,14 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 	
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -57,7 +59,7 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
-	public Post() {
+	public Post(Object object, Date date2, String string, String string2, AuthorDTO author) {
 		
 	}
 	@Override
@@ -84,8 +86,4 @@ public class Post implements Serializable {
 		return true;
 	}
 	
-	
-	
-	
-
 }
